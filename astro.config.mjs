4b7a5044b4653f89fb601e557d68e53eca655306
roblesdotdev <httpsnip@gmail.com>
2024-11-18 20:26:09 -0300

    setup tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 3000,
   },
+
+  integrations: [tailwind({ applyBaseStyles: false })],
 });
