b00db7a06c6d78ae2e694581601f67bd69ed53e5
roblesdotdev <httpsnip@gmail.com>
2024-11-18 21:34:52 -0300

    setup theme font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+import { type Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -12,7 +14,11 @@ export default {
         '2xl': '80rem',
       },
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Space Grotesk Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+} satisfies Config
