2e78ebd4ab94e5b60aca1dc36d101d20835498b0
roblesdotdev <httpsnip@gmail.com>
2024-11-18 21:04:48 -0300

    initial layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,16 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        '2xl': '2rem',
+      },
+      screens: {
+        '2xl': '80rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
